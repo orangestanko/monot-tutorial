5ecd78f3c6ce45b7629458c93643c9c4fc552475
orangestanko <silvestrstanko@orangefactory.cz>
2018-03-27 13:44:37 +0200

    color change

--- BasicLoop/Game1.cs
+++ BasicLoop/Game1.cs
@@ -97,7 +97,7 @@ namespace BasicLoop
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Red);
 
             spriteBatch.Begin();
             spriteBatch.Draw(texture, position);
